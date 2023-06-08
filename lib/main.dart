f71396e432653bf7419c1502cd1e582ce83143d0
unknown <traishitech.com@gmail.com>
2023-06-08 09:13:29 +0700

    แก้CMหลังพบลูกค้า

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ void main() {
 //----(Git Lab)
 //1.----> git add .
 //2.------> git remote set-url origin https://gitlab.com/traishitech.com/chaoperty.git
-//3.-------->git commit -m "commit message"
+//3.-------->git commit -m "แก้CMหลังพบลูกค้า"
 //4.-----------> git push origin main
 //----------------------------------------------------->
 //----(Git Hub)
